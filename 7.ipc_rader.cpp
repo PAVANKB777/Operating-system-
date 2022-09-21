5ac018fce277f07c035c0ef85f5e52630e017b07
PAVANKB777 <113426332+PAVANKB777@users.noreply.github.com>
2022-09-21 13:39:21 +0530

    Add files via upload

--- 7.ipc_rader.cpp
+++ 7.ipc_rader.cpp
@@ -6,21 +6,21 @@ int main()
 {
 	
 	key_t key = ftok("shmfile",65);
-
 	
 	int shmid = shmget(key,1024,0666|IPC_CREAT);
 
 	
 	char *str = (char*) shmat(shmid,(void*)0,0);
 
-	printf("Write Data : ");
-	scanf("%s",str);
-
-	printf("Data written in memory: %s\n",str);
+	printf("Data read from memory: %s\n",str);
 	
 	
 	shmdt(str);
-
+	
+	
+	shmctl(shmid,IPC_RMID,NULL);
+	
 	return 0;
 }
+
 
